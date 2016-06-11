24af1f333d5906cf78037f974c3228a7a75383b5
zhuoyikang <zhuoyikang@gmail.com>
2016-06-11 16:16:15 +0800

    x

---
+++ Assets/GameDefine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameDefine : MonoBehaviour {
+
+	public static Vector2 matrix = new Vector2(100,100);
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (matrix.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
