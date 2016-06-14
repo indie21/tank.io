e248588e8b37ed7f89aba457ac22f50c6a0092ed
zhuoyikang <zhuoyikang@gmail.com>
2016-06-14 22:53:02 +0800

    more socket

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using proto.packet;
+//using System.Collections;
 
 
 public class Player : MonoBehaviour {
 
     private int m_level = 1;
     private Vector3 m_Move;
-    public float m_Speed = 0.1f;
     private SpriteRenderer m_spriteRender;
     private Transform m_transform;
+    public float _speed = 0.1f;
+
 
     void Test() {
     }
-
        // Use this for initialization
     void Start () {
         m_spriteRender = GetComponent<SpriteRenderer> ();
@@ -30,27 +31,39 @@ public class Player : MonoBehaviour {
         Input.GetKey("w");
 
         if (Input.GetKey ("w")) {
-            m_Move = Vector3.up * m_Speed;
+            m_Move = Vector3.up * _speed;
             Move ();
         } else if (Input.GetKey ("s")) {
-            m_Move = Vector3.down * m_Speed;
+            m_Move = Vector3.down * _speed;
             Move ();
         } else if (Input.GetKey ("a")) {
-            m_Move = Vector3.left * m_Speed;
+            m_Move = Vector3.left * _speed;
             Move ();
         }
         else if (Input.GetKey ("d")) {
-            m_Move = Vector3.right * m_Speed;
+            m_Move = Vector3.right * _speed;
             Move ();
+        }
+
+
+        if (Input.GetKey("c")){
+            GameManager._netSocket.Connect("127.0.0.1", 8881);
+        }
+
+        if (Input.GetKey("s")) {
+            var packet = new packet();
+            // protoOut.Id = 10046;
+            // protoOut.name= "beitown";
+            // byte[] bytes = Serialize(protoOut);
         }
 
     }
 
     void LevelUp() {
         m_level += 1;
-        string name = string.Format ("Tank_{0:D2}", m_level);
-        Debug.Log ("name:" + name);
-        Sprite levelSprite = Resources.Load (name, typeof(Sprite)) as Sprite;
+        var name_1 = string.Format ("Tank_{0:D2}", m_level);
+        Debug.Log ("name:" + name_1);
+        var levelSprite = Resources.Load (name_1, typeof(Sprite)) as Sprite;
         m_spriteRender.sprite = levelSprite;
     }
 
